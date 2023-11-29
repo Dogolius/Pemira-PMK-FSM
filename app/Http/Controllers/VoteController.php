1d1449d904ebd1c7b6c46405a24b8c5282084d0a
Julius Adrian <juliusa368@gmail.com>
2023-11-29 17:43:39 +0700

    Set closing time for the votes

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -12,11 +12,14 @@ class VoteController extends Controller
     //
     public function store(Request $request)
     {
+        // Set the start date and time
+        $startDateTime = Carbon::create(2023, 12, 4, 8, 0, 0, 'Asia/Jakarta');
+
+        // Set the end date and time
+        $endDateTime = Carbon::create(2023, 12, 7, 15, 0, 0, 'Asia/Jakarta');
+
+        // Get the current date and time
         $currentDateTime = Carbon::now('Asia/Jakarta');
-        // Set the start time
-        $startDateTime = Carbon::create(2023, 11, 25, 2, 27, 0, 'Asia/Jakarta');
-        // Set the end time
-        $endDateTime = Carbon::create(2023, 11, 25, 2, 29, 0, 'Asia/Jakarta');
 
         if ($currentDateTime < $startDateTime) {
             // Display a message or redirect, indicating that the voting period is closed
@@ -27,23 +30,34 @@ class VoteController extends Controller
             // Display a message or redirect, indicating that the voting period is closed
             return redirect('/dashboard/vote')->with('voteError', 'Voting telah ditutup');
         }
-        $validatedData = $request->validate([
-            'voter_choose' => 'required'
-        ]);
-        $newVoteData = [
-            'voter_id' => auth()->user()->id,
-            'voter_name' => auth()->user()->name,
-            'voter_nim' => auth()->user()->nim,
-            'voter_choose' => $validatedData['voter_choose'],
-        ];
 
-        Vote::create($newVoteData);
-        $updatedData = [
-            'has_voted' => true
-        ];
 
-        User::where('id', auth()->user()->id)->update($updatedData);
-        return redirect('/dashboard/vote')->with('success', 'Voting berhasil dilakukan');
-        return redirect('/dashboard/vote')->with('success', 'Harap voting dalam periode waktu yang telah ditentukan');
+        // Check if the current time is within the specified time range (8:00 AM to 3:00 PM)
+        if ($currentDateTime->format('H:i') >= '08:00' && $currentDateTime->format('H:i') <= '15:00') {
+            // Allow the user to vote
+            $validatedData = $request->validate([
+                'voter_choose' => 'required'
+            ]);
+            $newVoteData = [
+                'voter_id' => auth()->user()->id,
+                'voter_name' => auth()->user()->name,
+                'voter_nim' => auth()->user()->nim,
+                'voter_choose' => $validatedData['voter_choose'],
+            ];
+    
+            Vote::create($newVoteData);
+            $updatedData = [
+                'has_voted' => true
+            ];
+    
+            User::where('id', auth()->user()->id)->update($updatedData);
+            return redirect('/dashboard/vote')->with('success', 'Voting berhasil dilakukan');
+        } else {
+            // Display a message or redirect, indicating that it's outside the allowed voting time
+            return redirect('/dashboard/vote')->with('voteError', 'Voting dibuka hanya dari jam 08:00 sampai 15:00');
+        }
+
+        
+
     }
 }
